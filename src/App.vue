3bb4568873d0ed5a9f25360de4577e7b36c92a8e
Georgia Stuart <georgia.stuart@gmail.com>
2020-03-19 17:17:33 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+        <v-toolbar-title>Random Data Generator</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <DataGenerator />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataGenerator from "@/components/DataGenerator";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+      DataGenerator
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/DataGenerator.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-btn @click="generateRandom()">Generate Random Number</v-btn>
+
+        <div>
+            {{ randomNumber }}
+        </div>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "DataGenerator",
+        data: function () {
+            return {
+                randomNumber: 0
+            }
+        },
+        methods: {
+            generateRandom: function () {
+                this.randomNumber = Math.random()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
